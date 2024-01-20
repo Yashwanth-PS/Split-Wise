0863b94dbc5503de60c86e2bcae177ce7d3413b3
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 16:11:58 +0530

    SplitWise - Created the Expense Repository

---
+++ src/main/java/com/splitwise/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.splitwise.repository;
+
+import com.splitwise.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
