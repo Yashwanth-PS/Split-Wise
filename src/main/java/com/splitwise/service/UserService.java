8828bfef58a70f9adb11bae99c886aaefd45d5f1
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 15:33:21 +0530

    SplitWise - Created the User Service Class

---
+++ src/main/java/com/splitwise/service/UserService.java
@@ -0,0 +1,31 @@
+package com.splitwise.service;
+
+import com.splitwise.model.User;
+import com.splitwise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUser(Long userId) throws Exception {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
+            throw new Exception("User Not Found!!!");
+        }
+        return userOptional.get();
+    }
+
+    public User registerUser(User user) {
+        Optional<User> savedUser = userRepository.save(user);
+        return savedUser.get();
+    }
+}
