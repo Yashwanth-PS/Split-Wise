4cc5e6e5e38257f8023f339d28ee8da8136786fc
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 14:55:30 +0530

    SplitWise - Updated the Register User Command Class

---
+++ src/main/java/com/splitwise/command/RegisterUserCommand.java
@@ -0,0 +1,41 @@
+package com.splitwise.command;
+
+import com.splitwise.controller.UserController;
+import com.splitwise.dto.RegisterUserRequestDTO;
+import com.splitwise.dto.RegisterUserResponseDTO;
+import com.splitwise.model.constant.ResponseStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component // Register User Command will have Controller --> Register User - name, phone, password
+public class RegisterUserCommand implements Command {
+    private final UserController userController;
+    @Autowired // @Repository, @Service, @RestController, @Component - When the Application starts Spring will create a Bean
+    public RegisterUserCommand(UserController userController) { // The User Controller Bean would be Injected here by Spring Dependency Injection
+        this.userController = userController;
+    }
+
+    @Override
+    public boolean canExecute(String input) {
+        return input.startsWith("registerUser") && input.split(" ").length == 4;
+    }
+
+    @Override
+    public void execute(String input) {
+        String[] arr = input.split(" ");
+        RegisterUserRequestDTO requestDTO = new RegisterUserRequestDTO();
+        requestDTO.setUserName(arr[1]);
+        requestDTO.setPhone(arr[2]);
+        requestDTO.setPassword(arr[3]);
+
+        RegisterUserResponseDTO responseDTO = userController.registerUser(requestDTO);
+
+        if (responseDTO.getResponseStatus() == ResponseStatus.SUCCESS) {
+            log.info("user created  with  id" + responseDTO.getUserId());
+        } else {
+            log.info("getUser  failed  with message= " + responseDTO.getMessage());
+        }
+    }
+}
