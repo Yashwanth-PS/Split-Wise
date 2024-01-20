4c2973964ee2e5e7713a3131056564b024385d1f
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 14:40:41 +0530

    SplitWise - Updated the Get User Response DTO Class

--- src/main/java/com/splitwise/dto/GetUserResponseDTO.java
+++ src/main/java/com/splitwise/dto/GetUserResponseDTO.java
@@ -1,8 +1,8 @@
 package com.splitwise.dto;
 
+import com.splitwise.model.constant.ResponseStatus;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
 @Setter
