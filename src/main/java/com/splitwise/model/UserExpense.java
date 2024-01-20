c1aae8887e872eb15af4c7e0e7deadb5bb86ac08
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 13:17:31 +0530

    SplitWise - Create the User Expense Model

---
+++ src/main/java/com/splitwise/model/UserExpense.java
@@ -0,0 +1,25 @@
+package com.splitwise.model;
+
+import com.Project.Splitwise.Models.Enum.UserExpenseType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "SPLITWISE_EXPENSEUSER")
+public class UserExpense extends BaseModel{
+    @ManyToOne
+    private User user;
+    private double amount;
+    @Enumerated(EnumType.ORDINAL)
+    private UserExpenseType userExpenseType;
+}
+
+/* User - UserExpense - 1:M -> Uni-Directional
+   Expense - UserExpense - 1:M -> Uni-Directional
+
+Expense:- For each Expense we want to know User Expense
+   Paid : A = 100, B = 100
+   Has To Pay : A = 50, B = 50, C = 100 */
