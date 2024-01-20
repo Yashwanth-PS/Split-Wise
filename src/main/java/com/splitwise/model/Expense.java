51f4d64067f7723c1f2ba040e1fb22cd871042bc
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 13:21:54 +0530

    SplitWise - Create the Expense Model

---
+++ src/main/java/com/splitwise/model/Expense.java
@@ -0,0 +1,35 @@
+package com.splitwise.model;
+
+import com.Project.Splitwise.Models.Enum.Currency;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity(name = "SPLITWISE_EXPENSE")
+public class Expense extends BaseModel{
+    private String description;
+    private double amount;
+    @OneToMany
+    private List<UserExpense> UserExpenses;
+    @Enumerated(EnumType.ORDINAL) // One expense will have one currency
+    private Currency currency;
+}
+
+/* Expense : UserExpense -> 1 : M {Uni-Directional on Expense side}
+   Group - Expense
+     1   -    M
+     1   -    1
+   Group : Expense -> 1 : M {Uni-Directional on Group side}
+{
+    "description" : "Dinner",
+    "amount" : 1000,
+    "group" : 1,
+    "currency" : "INR",
+    "paid" : [{"Yash" : 500}, {"Abhi" : 500}],
+    "hastToPay" : [{"Yash" : 250}, {"Abhi" : 250}, {"Prajwal" : 250}, {"Sachin" : 250}]
+} */
