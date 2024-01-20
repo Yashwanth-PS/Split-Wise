661699f670a17b0cf6eff6d5da93aa0563a1894d
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 21:58:48 +0530

    SplitWise - Updated the GroupServiceImpl Class

--- src/main/java/com/splitwise/service/GroupServiceImpl.java
+++ src/main/java/com/splitwise/service/GroupServiceImpl.java
@@ -2,7 +2,6 @@ package com.splitwise.service;
 
 import com.splitwise.dto.TransactionDTO;
 import com.splitwise.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +9,12 @@ import java.util.List;
 @Service
 public class GroupServiceImpl implements GroupService{
     // Ideal way to call a method in userService that calls the userRepository to get the Users
-    @Autowired
-    UserRepository userRepository;
+    final UserRepository userRepository;
+
+    public GroupServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<TransactionDTO> settleUp() {
         return null;
