af536c7a17426ffd5f5f416572a45c9ec70b1ae8
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 15:22:55 +0530

    SplitWise - Updated the Group Controller Class

--- src/main/java/com/splitwise/controller/GroupController.java
+++ src/main/java/com/splitwise/controller/GroupController.java
@@ -1,14 +1,17 @@
 package com.splitwise.controller;
 
+import com.splitwise.dto.SettleGroupRequestDTO;
+import com.splitwise.dto.SettleGroupResponseDTO;
+import com.splitwise.model.constant.ResponseStatus;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class GroupController {
 
-    public  SettleGroupResponseDto settleGroup(SettleGroupRequestDto requestDto){
-        SettleGroupResponseDto  responseDto  = new SettleGroupResponseDto();
-        responseDto.setResponseStatus(ResponseStatus.FAILURE);
-        responseDto.setMessage("not  implemented  yet");
-        return  responseDto;
+    public SettleGroupResponseDTO settleGroup(SettleGroupRequestDTO requestDTO) {
+        SettleGroupResponseDTO responseDTO = new SettleGroupResponseDTO();
+        responseDTO.setResponseStatus(ResponseStatus.FAILURE);
+        responseDTO.setMessage("Not Implemented Yet!!!");
+        return responseDTO;
     }
 }
