129a20b21d3999de4bc94ceea575da375dcc683f
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 13:19:15 +0530

    SplitWise - Create the User Model

---
+++ src/main/java/com/splitwise/model/User.java
@@ -0,0 +1,28 @@
+package com.splitwise.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity(name = "SPLITWISE_USER")
+@Builder
+public class User extends BaseModel{
+    private String name;
+    private String email;
+    private String phoneNumber;
+    @ManyToMany
+    private List<Group> groups;
+}
+
+/* User - Group : M:M -> Bidirectional
+   User - UserExpense : 1:M
+
+Expense:
+   Paid : A = 100, B = 100
+   - We want to know how much each user has spent or has to spend
+   - We want to know the amount and user from each userExpense Object
+   Has To Pay : A = 50, B = 50, C = 100 */
