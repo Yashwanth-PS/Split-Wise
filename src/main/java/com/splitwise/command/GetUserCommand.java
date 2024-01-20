a2eeeeb78a5d1538f476838427773f5b0f97a68e
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 14:45:26 +0530

    SplitWise - Updated the Get User Command Class

--- src/main/java/com/splitwise/command/GetUserCommand.java
+++ src/main/java/com/splitwise/command/GetUserCommand.java
@@ -1,15 +1,21 @@
 package com.splitwise.command;
 
+import com.splitwise.controller.UserController;
+import com.splitwise.dto.GetUserRequestDTO;
+import com.splitwise.dto.GetUserResponseDTO;
+import com.splitwise.model.constant.ResponseStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Slf4j // This is a Lombok Annotation which will generate a Logger for this class
 @Component // Get User Command will have User Controller --> GetUser using userId
 public class GetUserCommand implements Command {
     private UserController userController;
 
     @Autowired
     // @Repository, @Service, @RestController, @Component - When the Application starts Spring will create a Bean
-    public GetUserCommand(UserController userController) { // what ever  object  created in the  groupcontroller will be passed over  here  by spring  Dependency injection( topoogical sort
+    public GetUserCommand(UserController userController) { // The User Controller Bean would be Injected here by Spring Dependency Injection
         this.userController = userController;
     }
 
@@ -31,16 +37,16 @@ public class GetUserCommand implements Command {
     public void execute(String input) {
         String[] arr = input.split(" ");
         long userId = Long.parseLong(arr[1]);
-        GetUserRequestDto requestDto = new GetUserRequestDto();
-        requestDto.setUserId(userId);
-//        GetUserResponseDto responseDto = userController.getUser(requestDto);// before  using the  get mapping and  requstparam
-        GetUserResponseDto responseDto = userController.getUser(userId);
-        if (responseDto.getResponseStatus() == ResponseStatus.SUCCESS) {
-            System.out.println("User Id = " + responseDto.getUserId());
-            System.out.println("User name = " + responseDto.getUserName());
-            System.out.println("User phone= " + responseDto.getPhone());
+        GetUserRequestDTO requestDTO = new GetUserRequestDTO();
+        requestDTO.setUserId(userId);
+        // GetUserResponseDTO responseDto = userController.getUser(requestDTO); // Before using the Get Mapping and RequestParam
+        GetUserResponseDTO responseDTO = userController.getUser(userId);
+        if (responseDTO.getResponseStatus() == ResponseStatus.SUCCESS) {
+            log.info("User Id = " + responseDTO.getUserId());
+            log.info("User name = " + responseDTO.getUserName());
+            log.info("User phone = " + responseDTO.getPhone());
         } else {
-            System.out.println("getUser  failed  with message= " + responseDto.getMessage());
+            log.info("getUser  failed  with message= " + responseDTO.getMessage());
         }
     }
 }
