facc2bb5ec5d97634b138b48d744adeffc6b5430
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 16:20:50 +0530

    SplitWise - Updated User Model

--- src/main/java/com/splitwise/model/User.java
+++ src/main/java/com/splitwise/model/User.java
@@ -2,14 +2,18 @@ package com.splitwise.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Entity(name = "SPLITWISE_USER")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User extends BaseModel{
     private String name;
     private String email;
