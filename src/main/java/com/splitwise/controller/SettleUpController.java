3f983b3989dd6e1dae76ea4aae0eef069fabfae8
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 18:12:25 +0530

    SplitWise - Created the Settle Up Controller

---
+++ src/main/java/com/splitwise/controller/SettleUpController.java
@@ -0,0 +1,31 @@
+package com.splitwise.controller;
+
+import com.splitwise.dto.TransactionDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SettleUpController {
+
+    @Autowired
+    private InitService initService;
+
+    @Autowired
+    private GroupService groupService;
+
+    @GetMapping("/init") // Get Call --> www.domain_name.com/hello, localhost:8080/hello
+    public ResponseEntity initialise() { // Response Entity --> Internally converts Object into JSON
+        initService.initialise();
+        return ResponseEntity.ok("DONE"); // HTTP Status is 200
+    }
+
+    @GetMapping("/settleup")
+    public ResponseEntity settleUp() { // Response Entity --> Internally converts Object into JSON
+        List<TransactionDTO> transactionDTOS = groupService.settleUp();
+        return ResponseEntity.ok(transactionDTOS); // HTTP Status is 200
+    }
+}
