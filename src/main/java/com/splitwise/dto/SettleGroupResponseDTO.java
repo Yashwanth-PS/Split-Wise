a8d932eac2ef33be7b435f04ce906024be42a50c
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-20 18:06:57 +0530

    SplitWise - Updated the Settle Group Response DTO Class

--- src/main/java/com/splitwise/dto/SettleGroupResponseDTO.java
+++ src/main/java/com/splitwise/dto/SettleGroupResponseDTO.java
@@ -1,6 +1,5 @@
 package com.splitwise.dto;
 
-import com.splitwise.model.Transaction;
 import com.splitwise.model.constant.ResponseStatus;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +9,7 @@ import java.util.List;
 @Getter
 @Setter
 public class SettleGroupResponseDTO {
-    private List<Transaction> transactions;
+    private List<TransactionDTO> transactions;
     private ResponseStatus responseStatus;
     private String message;
 }
