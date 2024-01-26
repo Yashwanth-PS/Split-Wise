0b4126119e63b89e00b4b508834ee772b877e23d
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-26 22:16:19 +0530

    SplitWise - Updated the Repository

--- src/main/java/com/splitwise/repository/ExpenseRepository.java
+++ src/main/java/com/splitwise/repository/ExpenseRepository.java
@@ -1,9 +1,13 @@
 package com.splitwise.repository;
 
 import com.splitwise.model.Expense;
+import com.splitwise.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByGroup(Group group);
 }
