b626e94840e1d5ce303c775e5dfef19247297dee
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-27 06:43:47 +0530

    SplitWise - Updated the Main Class

--- src/main/java/com/splitwise/SplitWiseApplication.java
+++ src/main/java/com/splitwise/SplitWiseApplication.java
@@ -6,8 +6,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SplitWiseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SplitWiseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SplitWiseApplication.class, args);
+    }
 
 }
+
+/* implements CommandLineRunner {
+	private InitService initService;
+	private CommandRegistry commandRegistry;
+
+	@Autowired
+	public SplitWiseApplication(CommandRegistry commandRegistry, InitService initService){
+		this.commandRegistry = commandRegistry;
+		this.initService = initService;
+	} */
+
+/* // Pre-Initialization
+	@Override
+	public void run(String... args) throws Exception {
+        initService.initialise();
+        System.out.println("PRINTING from COMMAND LINE RUNNER");
+
+        // Input for the Application
+        // SettleGroup using groupId
+        // RegisterUser using name, phoneNumber, password
+        // GetUser using userId
+        // All the inputs are coming through this run method
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+			String input = sc.nextLine();
+			commandRegistry.process(input);
+		}
+    } */
